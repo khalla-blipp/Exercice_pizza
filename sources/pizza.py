1fd47f1f38531461ca8d8eedc2b510c294ea4967
Amadou Diallo <khala90@outlook.fr>
2021-10-25 11:15:39 +0200

    Ajout d'une nouvelle pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+   'my pizza': {
+        'ingredients': {'garlic': 3, 'pepper': 4, 'oregano': 3},
+        'price': 11,
     }
 }
